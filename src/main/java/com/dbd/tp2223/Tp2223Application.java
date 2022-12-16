f8dcbf1a19727c2c27c16ec0cfb7460a8ccab694
fedediclaudio <federicodicla@gmail.com>
2022-12-16 11:53:24 -0300

    Initial project

---
+++ src/main/java/com/dbd/tp2223/Tp2223Application.java
@@ -0,0 +1,13 @@
+package com.dbd.tp2223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2223Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbd/tp2223/Tp2223ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbd.tp2223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp2223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
